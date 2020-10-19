0d386fe22325efbed1890c81449c088b6af6b370
ZubSecurity <30393053+ZubSecurity@users.noreply.github.com>
2020-10-19 19:07:02 +0530

    size_of_binary_tree_updated

--- size_of_binary_tree.java
+++ size_of_binary_tree.java
@@ -3,11 +3,8 @@ class Tree
     
     public static int getSizeUtil(Node root){
          if(root == null) return 0;
-        
         int x = (root.left != null) ? 1 : 0;
         int y = (root.right != null) ? 1: 0;
-    
-            
         return x + y + getSizeUtil(root.left) + getSizeUtil(root.right);
     }
 	public static int getSize(Node root)
